4b92adb598372d1fbdf7225291386fdda7c0a47f
lo-opix <67984791+lo-opix@users.noreply.github.com>
2024-10-19 19:18:14 +0200

    Update the arduino script

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,60 +1,94 @@
-const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A3, A2, A1, A0, A10};
-const int NUM_BUTTONS = 6;
-const int buttonInputs[NUM_BUTTONS] = {9,8,7,6,5,4};
+#include <math.h>
+#include <ezButton.h>
+
+#define BUTTON_PIN_1 12  // The ESP32 pin GPIO25 connected to the button 1
+#define BUTTON_PIN_2 33  // The ESP32 pin GPIO26 connected to the button 2
+#define BUTTON_PIN_3 32  // The ESP32 pin GPIO27 connected to the button 3
+#define LED_PIN 25
+
+ezButton button1(BUTTON_PIN_1);  // create ezButton object for button 1
+ezButton button2(BUTTON_PIN_2);  // create ezButton object for button 2
+ezButton button3(BUTTON_PIN_3);  // create ezButton object for button 3
+
+bool ledIsOn = false;
+const int NUM_SLIDERS = 3;
+const int analogInputs[NUM_SLIDERS] = { 14, 27, 26 };
+const int MAX_VALUE = 1050;
 
 int analogSliderValues[NUM_SLIDERS];
-int buttonValues[NUM_BUTTONS];
+int newAnalogSliderValues[NUM_SLIDERS];
 
 void setup() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
   }
-
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    pinMode(buttonInputs[i], INPUT_PULLUP);
-  }
+  button1.setDebounceTime(100);  // set debounce time to 100 milliseconds
+  button2.setDebounceTime(100);  // set debounce time to 100 milliseconds
+  button3.setDebounceTime(100);  // set debounce time to 100 milliseconds
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  updateSliderValues();
-  sendSliderValues(); // Actually send data (all the time)
-//   printSliderValues(); // For debug
+  button1.loop();
+  button2.loop();
+  button3.loop();
+
+  if (updateSliderValues()) {  //Send data only if +- 1%
+    sendSliderValues();
+  }
+
+  if (button1.isPressed()){
+    Serial.println("The button 1 is pressed");
+    if (!ledIsOn){
+      digitalWrite(LED_PIN, HIGH);
+      ledIsOn = true;
+    }else{
+      digitalWrite(LED_PIN, LOW);
+      ledIsOn = false;
+    }
+  }
+
+  if (button2.isPressed())
+    Serial.println("The button 2 is pressed");
+
+  if (button3.isPressed())
+    Serial.println("The button 3 is pressed");
+
+  // printSliderValues(); // For debug
   delay(10);
 }
 
-void updateSliderValues() {
+bool updateSliderValues() {
+  bool update = false;
   for (int i = 0; i < NUM_SLIDERS; i++) {
-    analogSliderValues[i] = analogRead(analogInputs[i]);
+    newAnalogSliderValues[i] = MAX_VALUE - roundf((analogRead(analogInputs[i]) * MAX_VALUE) / 4095);
+    if (analogSliderValues[i] != 0) {
+      if (newAnalogSliderValues[i] > analogSliderValues[i] + 10 || newAnalogSliderValues[i] < analogSliderValues[i] - 10) {
+        if(newAnalogSliderValues[i] <= 10){
+          newAnalogSliderValues[i] = 0;
+        }else if (newAnalogSliderValues[i] >= MAX_VALUE - 10) {
+          newAnalogSliderValues[i] = MAX_VALUE;
+        }
+        analogSliderValues[i] = newAnalogSliderValues[i];
+        update = true;
+      }
+    } else {
+      analogSliderValues[i] = newAnalogSliderValues[i];
+    }
   }
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    buttonValues[i] = digitalRead(buttonInputs[i]);
-  }
+  return update;
 }
 
 void sendSliderValues() {
   String builtString = String("");
 
   for (int i = 0; i < NUM_SLIDERS; i++) {
-    builtString += "s";
     builtString += String((int)analogSliderValues[i]);
 
     if (i < NUM_SLIDERS - 1) {
-      builtString += String("|");
-    }
-  }
-
-  if(NUM_BUTTONS > 0){
-    builtString += String("|");
-  }
-
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    builtString += "b";
-    builtString += String((int)buttonValues[i]);
-
-    if (i < NUM_BUTTONS - 1) {
       builtString += String("|");
     }
   }
